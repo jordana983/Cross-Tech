64dbcc39ac75806ac60895aaba365f711dd3dded
JuliaHown <juliahown87@gmail.com>
2023-05-28 22:50:45 -0300

    Adicionando as validações

---
+++ PalavraCruzada.js
@@ -0,0 +1,14 @@
+function resultado(input) {
+    var valor = input.value.toUpperCase();
+    var id = input.id;
+
+    if (valor == id) {
+        input.value = `${input.value}`;
+        input.style = `background-color: #a4df66; color:black; border: 1px solid #FFF`;
+    } else {
+        input.style = `background-color: #c51e1c; border: 1px solid #FFF`;
+        input.value = ``;
+    }
+}
+
+btnMode.addEventListener('click', changeMode);
